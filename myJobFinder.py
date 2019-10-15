83e07a3e3f0ad7ee06d7979d69a9acfb29bf14b2
Sekou Keita <30804557+sekoukeita@users.noreply.github.com>
2019-10-15 01:46:27 -0400

    Fix no job found case

--- myJobFinder.py
+++ myJobFinder.py
@@ -3,8 +3,7 @@
 
 from selenium import webdriver
 from twilio.rest import Client
-import requests, os, datetime, webbrowser, openpyxl, pyautogui, twilioTextMe
-
+import requests, os, datetime, webbrowser, openpyxl, pyautogui, time
 
 pyautogui.PAUSE = 0.5 # allow 5 seconds pause between pyautogui function calls.
 
@@ -90,75 +89,86 @@ jobDateDropdownElt.click()
 print('Finding jobs...')
 submitElt.click()# submit the form
 browser.refresh()# refresh the browser to let it load all elements.
+searchResultElt = browser.find_element_by_css_selector('h1.pivot') # this elt is defined after the submit button because it is on the 2nd page.
 
-# click the load more elt button to display all jobs.
-while True:
-    try:
-        loadMoreJobElt = browser.find_element_by_id('loadMoreJobs') # button that is clicked to expand job list.
-        loadMoreJobElt.is_displayed() # return True or an exception. An exception is returned when the job list is all expanded (no more load button is displayed.) 
-        loadMoreJobElt.click() # if the button is displayed, it is clicked and more job are loaded.
-    except:
-        jobListElts = browser.find_elements_by_css_selector('div.summary > header > h2 > a')# list of all elts matching the css selector.
-        print('%s jobs links have been found for this search.\nDownloading...' %(len(jobListElts)))
-        break
+# check if some jobs have been found and download them.
+if searchResultElt.text != "Sorry, we didn't find any jobs matching your criteria":
+    while True:
+        try: # click the load more elt button to display all jobs.
+            loadMoreJobElt = browser.find_element_by_id('loadMoreJobs') # button that is clicked to expand job list.
+            loadMoreJobElt.is_displayed() # return True or an exception. An exception is returned when the job list is all expanded (no more load button is displayed.) 
+            loadMoreJobElt.click() # if the button is displayed, it is clicked and more job are loaded.
+        except:
+            jobListElts = browser.find_elements_by_css_selector('div.summary > header > h2 > a')# list of all elts matching the css selector.
+            print('%s jobs links have been found for this search.\nDownloading...' %(len(jobListElts)))
+            break
 
-# download jobs in html format and save them into a new folder.
-newFolder = 'Jobs_%s_for_%s' %(datetime.date.today().strftime("%a_%b_%d"), postingDate)
-os.makedirs(newFolder, exist_ok=True)
+    # download jobs in html format and save them into a new folder.
+    newFolder = 'Jobs_%s_for_%s' %(datetime.date.today().strftime("%a_%b_%d"), postingDate) # E.g: Jobs_Mon_Oct_14_for_Yesterday
+    os.makedirs(newFolder, exist_ok=True)
 
-jobFound = 0
-for elt in jobListElts: 
-    print('Downloading job_%s...' %(jobListElts.index(elt) + 1))
-    try:
-        res = requests.get(elt.get_attribute('href'))
-        res.raise_for_status()
-    except:
-        print("Couldn't dowmload this job")
-        continue
-    fileObj = open(os.path.join(newFolder, 'job_%s.html' %(jobListElts.index(elt) + 1)), 'wb')
-    
-    for chunk in res.iter_content(10000):
-        fileObj.write(chunk)
-    fileObj.close()
-    jobFound += 1
-print('%s jobs have been found and dowloaded for this search in the folder %s' %(jobFound, newFolder))
+    jobFound = 0
+    for elt in jobListElts: 
+        print('Downloading job_%s...' %(jobListElts.index(elt) + 1))
+        try:
+            res = requests.get(elt.get_attribute('href'))
+            res.raise_for_status()
+        except:
+            print("Couldn't dowmload this job")
+            continue
+        fileObj = open(os.path.join(newFolder, 'job_%s.html' %(jobListElts.index(elt) + 1)), 'wb')
+        
+        for chunk in res.iter_content(10000):
+            fileObj.write(chunk)
+        fileObj.close()
+        jobFound += 1
+    print('%s jobs have been found and dowloaded for this search in the folder %s' %(jobFound, newFolder))
+    jobs = True # boolean value that shows if any job has been found for the criteria provided.
+else:
+    print("Sorry, we didn't find any jobs matching your criteria")
+    jobs = False 
 
 # sending messages to the job seeker's phone.
 def textMe(message):
-    # Preset values. Replace them by your own twilio account information.
+    # Preset values.
     accountSID = 'yourTwilioAccountID'
     authToken = 'yourTwilioAuthToken'
     fromNumber = 'yourTwilioNumber'
-    
     toNumber = ws['D18'].value
     
     client = Client(accountSID, authToken)
     client.messages.create(body=message, from_=fromNumber, to=toNumber)
-    
-jobUrlList = []
-for elt in jobListElts:
-    jobUrlList.append(elt.get_attribute('href')) # list of jobs' url.
 
-message = "%s jobs, posted for %s since %s in %s have been found.\nCheck at %s on your computer for jobs' description." \
-           %(len(jobUrlList), jobTitle, postingDate, jobLocation, os.path.abspath(newFolder)) 
-textMe(message)
+if jobs:
+    jobUrlList = []
+    for elt in jobListElts:
+        jobUrlList.append(elt.get_attribute('href')) # list of jobs' url.
 
-# send a message containing up to 5 jobs description links among all the jobs found.
-maxJob = 5
-if len(jobUrlList) <= maxJob:
-    maxJob = len(jobUrlList)
-    
-if len(jobUrlList) > maxJob:
-    message = "Check the %s first job(s)' description:\n" %(maxJob)
-    links = ''
-    for i in range(maxJob):
-        links += jobUrlList[i] + '\n'
-    message += links
+    message = "%s jobs, posted for %s since %s in %s have been found.\nCheck at %s on your computer for jobs' description." \
+            %(len(jobUrlList), jobTitle, postingDate, jobLocation, os.path.abspath(newFolder))
+    textMe(message)
+
+    # send a message containing up to 5 jobs description links among all the jobs found.
+    maxJob = 5
+    if len(jobUrlList) <= maxJob:
+        maxJob = len(jobUrlList)
+        
+    if len(jobUrlList) > maxJob:
+        message = "Check the %s first job(s)' description:\n" %(maxJob)
+        links = ''
+        for i in range(maxJob):
+            links += jobUrlList[i] + '\n'
+        message += links
+    else:
+        message = "Check the %s job(s)' description:\n" %(maxJob)
+        links = ''
+        for i in range(maxJob):
+            links += jobUrlList[i] + '\n'
+        message += links
+    textMe(message)
 else:
-    message = "Check the %s job(s)' description:\n" %(maxJob)
-    links = ''
-    for i in range(maxJob):
-        links += jobUrlList[i] + '\n'
-    message += links
-textMe(message)
+   message = "Sorry, no job has been posted for %s since %s in %s." %(jobTitle, postingDate, jobLocation)
+   textMe(message)
+   
+time.sleep(5) # make a 5 seconds pause to allow the user to read messages before closing the program.
 
